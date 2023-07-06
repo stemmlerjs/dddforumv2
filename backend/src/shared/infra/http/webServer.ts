441805cf3b4fe02f697eb3d2d88ab5cf55701d0c
Khalil Stemmler <metroidman12@gmail.com>
2023-07-06 18:49:59 -0400

    Refactored to process service & added startup scripts.

--- backend/src/shared/infra/http/webServer.ts
+++ backend/src/shared/infra/http/webServer.ts
@@ -1,46 +1,8 @@
 import express from "express";
 import { Server } from "http";
 import { UserController } from "../../../modules/users/userController";
+import { ProcessService } from "../processes/processService";
 const cors = require("cors");
-const { exec } = require("child_process");
-
-function killProcessOnPort(port: number) {
-  const killCommand =
-    process.platform === "win32"
-      ? `netstat -ano | findstr :${port} | findstr LISTENING`
-      : `lsof -i:${port} -t`;
-
-  exec(killCommand, (error: any, stdout: any, stderr: any) => {
-    if (error) {
-      console.error(`Failed to execute the command: ${error.message}`);
-      return;
-    }
-
-    if (stderr) {
-      console.error(`Command execution returned an error: ${stderr}`);
-      return;
-    }
-
-    const processId = stdout.trim();
-    if (processId) {
-      const killProcessCommand =
-        process.platform === "win32"
-          ? `taskkill /F /PID ${processId}`
-          : `kill ${processId}`;
-
-      exec(killProcessCommand, (error: any, stdout: any, stderr: any) => {
-        if (error) {
-          console.error(`Failed to kill the process: ${error.message}`);
-          return;
-        }
-
-        console.log(`Process running on port ${port} has been killed.`);
-      });
-    } else {
-      console.log(`No process found running on port ${port}.`);
-    }
-  });
-}
 
 type WebServerConfig = {
   port: number;
@@ -89,12 +51,13 @@ export class WebServer {
   }
 
   async start(): Promise<void> {
-    killProcessOnPort(3000);
     return new Promise((resolve, reject) => {
-      this.server = this.express.listen(this.config.port, () => {
-        console.log(`Server is running on port ${this.config.port}`);
-        this.started = true;
-        resolve();
+      ProcessService.killProcessOnPort(this.config.port, () => {
+        this.server = this.express.listen(this.config.port, () => {
+          console.log(`Server is running on port ${this.config.port}`);
+          this.started = true;
+          resolve();
+        });
       });
     })
   }
