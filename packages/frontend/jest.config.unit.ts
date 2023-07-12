5c87c9e61166cf051e572040fe843897792da79a
Kirill Karpov <karpov.kir.v@gmail.com>
2023-07-12 08:11:46 +0400

    Add setupFiles to unit frontend tests config

--- packages/frontend/jest.config.unit.ts
+++ packages/frontend/jest.config.unit.ts
@@ -17,4 +17,5 @@ export default async (): Promise<JestConfigWithTsJest> => ({
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
   testEnvironment: 'jest-environment-jsdom',
+  setupFiles: ['./tests/testsSetup.ts'],
 });
