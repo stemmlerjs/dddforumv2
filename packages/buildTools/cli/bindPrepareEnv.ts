f246b7416f8c4610027d2eb2ddd8abbcf8f07119
Kirill Karpov <karpov.kir.v@gmail.com>
2023-07-15 23:41:04 +0400

    Rename dockerComposePath to dockerComposeFilePath

--- packages/buildTools/cli/bindPrepareEnv.ts
+++ packages/buildTools/cli/bindPrepareEnv.ts
@@ -6,11 +6,11 @@ export const bindPrepareEnv = (program: Command) => {
   return program
     .command('prepareEnv')
     .description('Prepare env e.g. for development')
-    .requiredOption('-d <dockerComposePath>', 'docker-compose file path')
+    .requiredOption('-d <dockerComposeFilePath>', 'docker-compose file path')
     .requiredOption('-o <ormSchemaPath>', 'orm schema file path')
-    .action(async ({ d: dockerComposePath, o: ormSchemaPath }) => {
+    .action(async ({ d: dockerComposeFilePath, o: ormSchemaPath }) => {
       await prepareEnv({
-        relativeDockerComposeFilePath: dockerComposePath,
+        relativeDockerComposeFilePath: dockerComposeFilePath,
         relativeOrmSchemaPath: ormSchemaPath,
       });
     });
