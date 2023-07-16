56576887848b903df223007d542b354779423f65
Kirill Karpov <karpov.kir.v@gmail.com>
2023-07-16 12:32:19 +0400

    Refine commands description

--- packages/cli/cli/bindBuild.ts
+++ packages/cli/cli/bindBuild.ts
@@ -5,7 +5,7 @@ import { build } from '../src/commands/build/build';
 export const bindBuild = (program: Command) => {
   return program
     .command('build')
-    .description('Build TS files with respect of TS aliases')
+    .description('Transpile TS files and post-process imports')
     .requiredOption('-c <tsconfigPath>', 'tsconfig path')
     .action(async ({ c: tsconfigPath }) => {
       await build({ tsconfigPath });
--- packages/cli/cli/bindPrepareEnv.ts
+++ packages/cli/cli/bindPrepareEnv.ts
@@ -5,7 +5,7 @@ import { prepareEnv } from '../src/commands/prepareEnv';
 export const bindPrepareEnv = (program: Command) => {
   return program
     .command('prepareEnv')
-    .description('Prepare env e.g. for development')
+    .description('Prepare environment e.g. for development')
     .requiredOption('-d <dockerComposeFilePath>', 'docker-compose file path')
     .requiredOption('-o <ormSchemaPath>', 'ORM schema file path')
     .action(async ({ d: dockerComposeFilePath, o: ormSchemaPath }) => {
