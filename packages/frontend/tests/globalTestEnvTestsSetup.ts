b68daefd0b99d2ed501ce337038761256e749546
Kirill Karpov <karpov.kir.v@gmail.com>
2023-07-19 10:19:28 +0400

    build: use ENV instead of NODE_ENV

--- packages/frontend/tests/globalTestEnvTestsSetup.ts
+++ packages/frontend/tests/globalTestEnvTestsSetup.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import path from 'path';
 
 export default async (): Promise<void> => {
-  const nodeEnv = process.env.NODE_ENV || 'development'
+  const nodeEnv = process.env.ENV || 'development';
   const envPath = path.join(__dirname, `../.env.${nodeEnv}`);
 
   console.log(`Starting tests using env `, envPath);
