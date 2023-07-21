587ce5468a246f7aa3784dd0e3c12c7dc6f4d758
Khalil Stemmler <metroidman12@gmail.com>
2023-07-21 01:46:26 -0400

    Added dist to .gitignore.

--- dist/shared/utils/numberUtils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateRandomInteger = void 0;
-function generateRandomInteger(min, max) {
-    return Math.floor(min + Math.random() * (max - min + 1));
-}
-exports.generateRandomInteger = generateRandomInteger;
